e8b398e8ea829d9f049dd0d8aeb4a63b000d0f61
Blarc <jakob.malezic@gmail.com>
2023-03-27 17:36:15 +0200

    feat: Compute diff only for selected files (#2).

--- src/main/kotlin/com/github/blarc/ai/commits/intellij/plugin/AICommitAction.kt
+++ src/main/kotlin/com/github/blarc/ai/commits/intellij/plugin/AICommitAction.kt
@@ -1,50 +1,49 @@
 package com.github.blarc.ai.commits.intellij.plugin
 
+import com.github.blarc.ai.commits.intellij.plugin.AICommitsBundle.message
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vcs.VcsDataKeys
-import git4idea.GitUtil
+import com.intellij.vcs.commit.AbstractCommitWorkflowHandler
+import git4idea.commands.Git
+import git4idea.commands.GitCommand
+import git4idea.commands.GitLineHandler
+import git4idea.repo.GitRepositoryManager
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.File
-import java.io.IOException
-import java.util.concurrent.TimeUnit
 
 class AICommitAction : AnAction(), DumbAware {
     @OptIn(DelicateCoroutinesApi::class)
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+
         val commitMessage = VcsDataKeys.COMMIT_MESSAGE_CONTROL.getData(e.dataContext)
+        val commitWorkflowHandler = e.getData(VcsDataKeys.COMMIT_WORKFLOW_HANDLER) as AbstractCommitWorkflowHandler<*, *>
 
-        val gitRepoManger = GitUtil.getRepositoryManager(e.project!!)
-        var diff = ""
-        gitRepoManger.repositories.forEach {
-            diff += "git diff --cached".runCommand(File(it.root.canonicalPath!!))
-        }
+        runBackgroundableTask(message("action.background"), project) {
+            val diff = commitWorkflowHandler.ui.getIncludedChanges().joinToString("\n") {
 
-        val instance = OpenAIService.instance
-        GlobalScope.launch(Dispatchers.Main) {
-            val generatedCommitMessage = instance.generateCommitMessage(diff, 1)
-            commitMessage?.setCommitMessage(generatedCommitMessage)
-        }
-    }
+                val repository = GitRepositoryManager.getInstance(project).getRepositoryForFile(it.virtualFile)
+                val diffCommand = GitLineHandler(
+                    project,
+                    repository!!.root,
+                    GitCommand.DIFF
+                )
+                diffCommand.addParameters("--cached", it.virtualFile!!.path)
 
-    private fun String.runCommand(workingDir: File): String? {
-        return try {
-            val parts = this.split("\\s".toRegex())
-            val proc = ProcessBuilder(*parts.toTypedArray())
-                .directory(workingDir)
-                .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                .redirectError(ProcessBuilder.Redirect.PIPE)
-                .start()
+                val commandResult = Git.getInstance().runCommand(diffCommand)
+                commandResult.outputAsJoinedString
+            }
 
-            proc.waitFor(60, TimeUnit.MINUTES)
-            proc.inputStream.bufferedReader().readText()
-        } catch(e: IOException) {
-            e.printStackTrace()
-            null
+            val openAIService = OpenAIService.instance
+            GlobalScope.launch(Dispatchers.Main) {
+                val generatedCommitMessage = openAIService.generateCommitMessage(diff, 1)
+                commitMessage?.setCommitMessage(generatedCommitMessage)
+            }
         }
     }
 }
