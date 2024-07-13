091ca1b03f09fceb1e85508b1b4a015e3b473755
Blarc <jakob.malezic@gmail.com>
2024-07-13 20:45:48 +0200

    feat(clients): support gemini

---
+++ src/main/kotlin/com/github/blarc/ai/commits/intellij/plugin/settings/clients/gemini/GeminiClientSharedState.kt
@@ -0,0 +1,31 @@
+package com.github.blarc.ai.commits.intellij.plugin.settings.clients.gemini
+
+import com.github.blarc.ai.commits.intellij.plugin.settings.clients.LLMClientSharedState
+import com.intellij.openapi.components.*
+import com.intellij.util.xmlb.XmlSerializerUtil
+import com.intellij.util.xmlb.annotations.XCollection
+
+@Service(Service.Level.APP)
+@State(name = "GeminiClientSharedState", storages = [Storage("AICommitsGemini.xml")])
+class GeminiClientSharedState : PersistentStateComponent<GeminiClientSharedState>, LLMClientSharedState {
+
+    companion object {
+        @JvmStatic
+        fun getInstance(): GeminiClientSharedState = service()
+    }
+
+    @XCollection(style = XCollection.Style.v2)
+    override val hosts = mutableSetOf("http://localhost:11434/")
+
+    @XCollection(style = XCollection.Style.v2)
+    override val modelIds: MutableSet<String> = mutableSetOf(
+        "gemini-pro",
+        "gemini-ultra"
+    )
+
+    override fun getState(): GeminiClientSharedState = this
+
+    override fun loadState(state: GeminiClientSharedState) {
+        XmlSerializerUtil.copyBean(state, this)
+    }
+}
